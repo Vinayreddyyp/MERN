5787911123e746163dc12da9e8c3f6e0fae1b802
vinayreddyyp <ypvinayreddy@gmail.com>
2022-03-23 10:11:31 -0700

    add delete functionality to the index

--- src/index.js
+++ src/index.js
@@ -44,6 +44,28 @@ app.get('/users/:id', async (req, res) => {
 	
 });
 
+app.patch('/users/:id', async(req, res) => {
+	const updates = Object.keys(req.body);
+	console.log('updates', updates);
+	const allowedUpdates = ['name', 'email', 'password', 'age'];
+	const isValidOperation = updates.every((update) => {
+		allowedUpdates.includes(update)
+	})
+
+	if(!isValidOperation) {
+		return res.status(400).send({error:'Invalid updates'});
+	}
+	try{
+		const user = await User.findByIdAndUpdate(req.params.id, req.body,{new:  true, runValidators:true});
+		console.log('user as per idxx', user)
+		if(!user) {
+			return res.status(404).send()
+		}
+		res.send(user)
+	} catch(e) {
+		res.status(400).status();
+	}
+})
 app.post('/tasks', async(req, res) => {
 	const tasks = new Task(req.body);
 
@@ -67,20 +89,61 @@ app.get('/tasks', async(req, res) => {
 	
 });
 
+app.patch('/tasks/:id', async(req, res) => {
+	const updates = Object.keys(req.body);
+	const allowedUpdates = ['description', 'completed'];
+	const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+	if(!isValidOperation) return res.status(400).send({error: 'Invalid operation'});
+	
+	try {
+		const tasks = await Task.findByIdAndUpdate(req.params.id, req.body,{new:  true, runValidators:true});
+		if(!tasks) return res.status(404).send();
+		res.send(tasks);
+		} catch(e) {
+			res.status(400).send(e)
+		}
+})
+
 app.get('/tasks/:id', async(req, res) => {
 	const _id = req.params.id;
 
-	// try {
-	// 	const task = await Task.findById(_id);
-	// 	if(!task) {
-	// 		res.status(404).send()
-	// 	}
-	// 	res.send(task);
-	// }catch(err) {
-    //    res.status(400).send(e)
-	// }
+	try {
+		const task = await Task.findById(_id);
+		if(!task) {
+			res.status(404).send()
+		}
+		res.send(task);
+	}catch(err) {
+       res.status(400).send(e)
+	}
 	
 });
+
+app.delete('/users/:id', async(req, res) => {
+	try {
+		const user = await User.findByIdAndDelete(req.params.id);
+		if(!user) {
+			return res.status(404).send({'Error': 'username not found'})
+		}
+		res.send(user)
+	} catch(e) {
+		res.status(500).send(e)
+	}
+})
+
+app.delete('/task/:id', async(req, res) => {
+	try {
+		const tasks = await Task.findByIdAndDelete(req.params.id);
+		if(!tasks) {
+			res.send(404).send({'Error': 'Task not found'});
+
+		}
+		res.send(tasks)
+	} catch(e) {
+		res.send(500).send(e)
+	}
+})
 
 app.listen(port, () => {
 	console.log('Server is up on port' + port);
