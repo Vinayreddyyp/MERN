87af60eae2ec0a30b73917db809150c904275318
vinayreddyyp <ypvinayreddy@gmail.com>
2022-03-29 15:43:00 -0700

    add bug fix to the user routers

--- src/routers/user.js
+++ src/routers/user.js
@@ -7,8 +7,10 @@ router.post('/users',async(req, res) => {
 	const user = new User(req.body);
 	
 	try {
-		await user.save()
-         res.status(201).send(user)
+		await user.save();
+        const token = await user.generateAuthToken();
+        
+         res.status(201).send({user , token});
 	} catch(e) {
      res.status(400).send(e);
 	}
