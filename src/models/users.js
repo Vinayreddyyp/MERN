ced34e4ecc3cc2c0a07698983d1dd5cc1e39bd3b
vinayreddyyp <ypvinayreddy@gmail.com>
2022-04-01 13:12:24 -0700

    add new config to user

--- src/models/users.js
+++ src/models/users.js
@@ -54,6 +54,7 @@ const userSchema = new mongoose.Schema({
 userSchema.methods.generateAuthToken = async function () {
     const user = this
     const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
+	console.log('token in the model', token)
 
     user.tokens = user.tokens.concat({ token })
     await user.save()
