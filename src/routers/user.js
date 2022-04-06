00476bb653caf994a02fef97d921b8a0fad6efda
vinayreddyyp <ypvinayreddy@gmail.com>
2022-04-06 11:27:20 -0700

    fix bug

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const User = require('../models/users');
+const express = require('express')
+const User = require('../models/user')
 const auth = require('../middleware/auth')
-const router = new express.Router();
-
+const router = new express.Router()
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
@@ -26,35 +25,6 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.post('/users/logoutAll', auth, async (req, res) => {
-    try {
-        req.user.tokens = [];
-        await req.user.save();
-        res.send()
-    } catch (e) {
-        res.status(500).send(e)
-    }
-});
-
-router.get('/users/me', auth, async (req, res) => {
-    res.send(req.user)
-})
-
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const user = await User.findById(_id)
-
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
 router.post('/users/logout', auth, async (req, res) => {
     try {
         req.user.tokens = req.user.tokens.filter((token) => {
@@ -68,8 +38,22 @@ router.post('/users/logout', auth, async (req, res) => {
     }
 })
 
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
 
-router.patch('/users/:id', async (req, res) => {
+router.get('/users/me', auth, async (req, res) => {
+    console.log('its executed');
+    res.send(req.user)
+})
+
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -79,33 +63,20 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findById(req.params.id)
-
-        updates.forEach((update) => user[update] = req.body[update])
-        await user.save()
-
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
+        res.send(req.user)
     } catch (e) {
         res.status(400).send(e)
     }
 })
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
-
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
+        await req.user.remove()
+        res.send(req.user)
     } catch (e) {
         res.status(500).send()
     }
 })
-
-module.exports = router;
+module.exports = router
