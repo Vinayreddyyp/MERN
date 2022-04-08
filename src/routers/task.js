21ecb9b1228f0b10122348db87e3109d69c64174
vinayreddyyp <ypvinayreddy@gmail.com>
2022-04-08 09:25:47 -0700

    add task config to the user task

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const Task = require('../models/task');
+const auth = require('../middleware/auth');
 const router = new express.Router();
 
 
 
-router.post('/tasks', async(req, res) => {
-	const tasks = new Task(req.body);
+router.post('/tasks',auth,  async(req, res) => {
+	console.log('id of the task', req.user._id)
+	//const tasks = new Task(req.body);
+	const tasks = new Task({
+		...req.body,
+		owner: req.user._id
+	})
 
 
 	try {
