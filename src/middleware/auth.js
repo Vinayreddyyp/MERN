a11f0bad490f488ab5cec71b7fd1cf85bcbb9491
vinayreddyyp <ypvinayreddy@gmail.com>
2022-04-08 09:22:33 -0700

    add task config to the auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,7 +5,8 @@ const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '')
         const decoded = jwt.verify(token, 'thisismynewcourse')
-        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
+        console.log('user in the auth', user);
 
         if (!user) {
             throw new Error()
