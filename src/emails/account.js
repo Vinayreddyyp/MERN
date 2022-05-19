4e5a0900bb923ba6febdd095d76968293499ef59
vinayreddyyp <ypvinayreddy@gmail.com>
2022-05-19 12:43:43 -0700

    remove the api key from the account.js

--- src/emails/account.js
+++ src/emails/account.js
@@ -7,10 +7,24 @@ sgMail.setApiKey(sendGridApiKey);
 
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
-to: email,
-from: 'ypvinayreddy@gmail.com',
- subject: 'this is email from vinay reddy',
- text: 'Welcome to the vinay desctibing chebalagudu beach',
+        to: email,
+        from: 'ypvinayreddy@gmail.com',
+        subject: 'this is email from vinay reddy',
+        text: 'Welcome to the vinay desctibing chebalagudu beach',
+        html: `welcome to the app, ${name}, have a great time`
+ 
  
     })
+} 
+
+const sendCancelationEmail = (email, name) => {
+       sgMail.send({
+           to:email,
+           from: 'ypvinayreddy@gmail.com',
+           subject: 'Sorry to see you g0!',
+           text: `Goodbye,${name}, I hope to see you back sometime soon`
+       })
+}
+module.exports = {
+    sendWelcomeEmail
 }
