7f749cfbe2054746ee84d08cf2f558669c0103d1
vinayreddyyp <ypvinayreddy@gmail.com>
2022-05-19 11:00:07 -0700

    add email sending logic to the user

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const multer = require('multer');
+const {sendWelcomeEmail} = require('../emails/account')
 
 const router = new express.Router()
 
@@ -10,6 +11,7 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save()
+        await sendWelcomeEmail(user.name, user.email);
         const token = await user.generateAuthToken()
         res.status(201).send({ user, token });
        
